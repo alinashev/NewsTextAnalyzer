e3d1162a41e98f4c19ff16239f999eceb30d2409
alinashev <al.t.shevchenko@gmail.com>
2022-08-30 13:57:48 +0300

    Removed extra commented code

--- project/transformation/snowflakeconf.py
+++ project/transformation/snowflakeconf.py
@@ -17,31 +17,3 @@ class SnowflakeConfigurator:
             "sfSchema": self.snowflake_schema,
             "sfWarehouse": self.snowflake_warehouse
         }
-
-
-"""
-from commons.secretsmanager import SecretsManager
-from pyspark.sql import SparkSession
-
-from snowflakeconf import SnowflakeConfigurator
-
-
-def main():
-    spark = SparkSession.builder.appName("App") \
-        .master("local[8]") \
-        .config("spark.driver.memory", "1g") \
-        .config("spark.executor.memory", "1g") \
-        .config("spark.memory.offHeap.enabled", True) \
-        .config("spark.memory.offHeap.size", "8g") \
-        .getOrCreate()
-
-    snowflake = SnowflakeConfigurator()
-    df = spark.read.format(snowflake.snowflake_source_name) \
-        .options(**snowflake.snowflake_configuration()) \
-        .option("dbtable", snowflake.source_table_name) \
-        .load()
-    df.show(n=3)
-
-
-if __name__ == "__main__":
-    main()"""
