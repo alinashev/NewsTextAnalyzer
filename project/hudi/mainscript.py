e0514aa437a428e4885e5a524b7de9e551521180
alinashev <al.t.shevchenko@gmail.com>
2022-08-30 19:05:36 +0300

    Updated Hudi-part

--- project/hudi/mainscript.py
+++ project/hudi/mainscript.py
@@ -13,7 +13,6 @@ from awsglue import DynamicFrame
 
 import boto3
 
-## @params: [JOB_NAME]
 args = getResolvedOptions(
     sys.argv,
     ["JOB_NAME", "database_name", "kinesis_table_name",
@@ -24,7 +23,9 @@ args = getResolvedOptions(
 spark = SparkSession.builder.config(
     'spark.serializer',
     'org.apache.spark.serializer.KryoSerializer'
-).config('spark.sql.hive.convertMetastoreParquet', 'false').getOrCreate()
+).config(
+    'spark.sql.hive.convertMetastoreParquet', 'false'
+).getOrCreate()
 
 sc = spark.sparkContext
 glueContext = GlueContext(sc)
@@ -36,11 +37,12 @@ kinesis_table_name = args["kinesis_table_name"]
 hudi_table_name = args["hudi_table_name"]
 s3_path_hudi = args["s3_path_hudi"]
 s3_path_spark = args["s3_path_spark"]
-starting_position_of_kinesis_iterator = args["starting_position_of_kinesis_iterator"]
+starting_position_of_kinesis_iterator = \
+    args["starting_position_of_kinesis_iterator"]
 window_size = args["window_size"]
 catalogue_table_name = database_name + '.' + hudi_table_name
 
-data_frame_from_catalog = glueContext.create_data_frame.from_catalog(
+data_frame_DataSource0 = glueContext.create_data_frame.from_catalog(
     database=database_name,
     table_name=kinesis_table_name,
     transformation_ctx="DataSource0",
@@ -50,7 +52,7 @@ data_frame_from_catalog = glueContext.create_data_frame.from_catalog(
     }
 )
 
-hudi_write_config = {
+hudiWriteConfig = {
     'className': 'org.apache.hudi',
     'hoodie.table.name': hudi_table_name,
     'hoodie.datasource.write.operation': 'upsert',
@@ -66,7 +68,7 @@ hudi_write_config = {
     'hoodie.deltastreamer.keygen.timebased.output.dateformat': 'yyyy/MM/dd'
 }
 
-hudi_glue_config = {
+hudiGlueConfig = {
     'hoodie.datasource.hive_sync.enable': 'true',
     'hoodie.datasource.hive_sync.sync_as_datasource': 'false',
     'hoodie.datasource.hive_sync.database': database_name,
@@ -91,7 +93,7 @@ def delete_extra_column(df):
     )
 
 
-def evolve_schema(kinesis_df, table):
+def evolveSchema(kinesis_df, table, forcecast=False):
     try:
         glue_catalog_df = delete_extra_column(
             get_existing_table_schema(table)
@@ -106,15 +108,16 @@ def evolve_schema(kinesis_df, table):
         return kinesis_df
 
 
-def processBatch(data_frame):
+def processBatch(data_frame, batchId):
     if data_frame.count() > 0:
         kinesis_dynamic_frame = DynamicFrame.fromDF(
             data_frame, glueContext, "from_kinesis_data_frame"
         ).toDF()
 
-        kinesis_data_frame = evolve_schema(
-            kinesis_dynamic_frame.toDF(),
-            catalogue_table_name
+        kinesis_data_frame = evolveSchema(
+            kinesis_dynamic_frame,
+            catalogue_table_name,
+            False
         )
 
         glueContext.write_dynamic_frame.from_options(
@@ -126,14 +129,14 @@ def processBatch(data_frame):
             connection_type="custom.spark",
             connection_options={
                 'path': s3_path_hudi,
-                **hudi_write_config,
-                **hudi_glue_config
+                **hudiWriteConfig,
+                **hudiGlueConfig
             }
         )
 
 
 glueContext.forEachBatch(
-    frame=data_frame_from_catalog,
+    frame=data_frame_DataSource0,
     batch_function=processBatch,
     options={
         "windowSize": window_size,
